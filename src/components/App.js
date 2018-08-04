787f3b200266686e7604f25822018b4dabc55eb5
Ross Young <ross.young12@gmail.com>
2018-08-03 23:30:54 -0500

    adds buttons to set state id value

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,41 @@ import './App.css';
 import Screen from "./Screen/Screen";
 import TextBox from "./TextBox/TextBox";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 15
+    };
+
+    this.nextPokemon = this.nextPokemon.bind(this);
+    this.prevPokemon = this.prevPokemon.bind(this);
+  }
+
+  nextPokemon() {
+    this.setState((prevState) => {
+    // Important: read `prevState` instead of `this.state` when updating.
+    return {id: prevState.id + 1}
+  });
+  }
+
+  prevPokemon() {
+    this.setState((prevState) => {
+    // Important: read `prevState` instead of `this.state` when updating.
+    return {id: prevState.id - 1}
+  });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="app-container">
           <div className="pokedex-left">
-            <Screen />
+            <Screen id={this.state.id}/>
+            <button onClick={this.prevPokemon}>Prev</button>
+            <button onClick={this.nextPokemon}>Next</button>
           </div>
           <div className="pokedex-right">
-            <TextBox />
+            <TextBox id={this.state.id}/>
           </div>
         </div>
       </div>
