5505aded570bbab20f9fa57e40326fc724920d1e
Ross Young <ross.young12@gmail.com>
2018-08-03 23:30:02 -0500

    updates img src to use props

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './Screen.css';
 
 class Screen extends Component {
+//   constructor() {
+//       super();
+//     //   this.state = {
+//     //       id: 1,
+//     //   }
+//   }
+
   render() {
     return (
       <div className="Screen">
-        <img className="pokemon-sprite" src={require('./../../images/19.png')} alt="bulbasaur"/>
+        <img className="pokemon-sprite" src={require(`./../../images/${this.props.id}.png`)} alt="bulbasaur"/>
       </div>
     );
   }
