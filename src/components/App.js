b8409e7275bd000efd335a186ca1e177fcb5b300
Ross Young <ross.young12@gmail.com>
2019-02-15 11:51:09 -0600

    updates App

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Screen from "./Screen/Screen";
 import TextBox from "./TextBox/TextBox";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 15
-    };
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: (Math.floor(Math.random() * 150) + 1)
+    };
+    this.firstPokemon = this.firstPokemon.bind(this);
     this.nextPokemon = this.nextPokemon.bind(this);
     this.prevPokemon = this.prevPokemon.bind(this);
+    this.lastPokemon = this.lastPokemon.bind(this);
   }
 
+  // nextPokemon() {
+  //   // console.log(this.props.id)
+  //   let newNumber = this.props.number + 1;
+  //   return newNumber;
+  // }
+
+  // prevPokemon() {
+  //   let newNumber = this.props.number - 1;
+  //   return newNumber;
+  // }
+
   nextPokemon() {
-    this.setState((prevState) => {
+    // console.log(this.props.number);
+    this.setState((prevState) => ({
     // Important: read `prevState` instead of `this.state` when updating.
-    return {id: prevState.id + 1}
-  });
+       id: prevState.id + 1
+    }));
   }
 
   prevPokemon() {
-    this.setState((prevState) => {
+    this.setState((prevState) => ({
     // Important: read `prevState` instead of `this.state` when updating.
-    return {id: prevState.id - 1}
-  });
+      id: prevState.id - 1
+    }));
+  }
+
+  firstPokemon() {
+    this.setState({id: 1})
+  }
+
+  lastPokemon() {
+    this.setState({id: 151})
   }
 
   render() {
@@ -33,10 +55,19 @@ class App extends Component {
       <div className="App">
         <div className="app-container">
           <div className="pokedex-left">
+            <div className="blue-light"></div>
+            <div className="blue-light-shadow"></div>
             <Screen id={this.state.id}/>
-            <button onClick={this.prevPokemon}>Prev</button>
-            <button onClick={this.nextPokemon}>Next</button>
+            <div className="buttons">
+              <div className="buttons_container">
+                <button onClick={this.firstPokemon}></button>
+                <button onClick={this.nextPokemon}></button>
+                <button onClick={this.prevPokemon}></button>
+                <button onClick={this.lastPokemon}></button>
+              </div>
+            </div>
           </div>
+          <div className="pokedex-spine"></div>
           <div className="pokedex-right">
             <TextBox id={this.state.id}/>
           </div>
